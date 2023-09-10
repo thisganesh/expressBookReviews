7ad43e7667ac8a576e73497fdf6ce2cd0f20137d
thisganesh <mswamiganesh@gmail.com>
2023-09-10 05:23:34 -0400

    add task 11,12,13,14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,7 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 const bodyParser = require("body-parser");
+const axios = require('axios');
 
 public_users.use(bodyParser.json())
 
@@ -38,6 +39,17 @@ public_users.get('/',function (req, res) {
   bookJson = JSON.stringify(books)
   return res.status(200).json({message: "These are the list of books",books:bookJson});
 });
+// using promise
+public_users.get('/', function (req, res) {
+    axios.get('./bookdb.js')
+      .then((response) => {
+        const books = response.data.books;
+        return res.status(200).json({ message: "These are the list of books", books });
+      })
+      .catch((error) => {
+        return res.status(500).json({ message: "Failed to fetch books", error: error.message });
+      });
+  });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
@@ -51,6 +63,25 @@ public_users.get('/isbn/:isbn',function (req, res) {
     }
   return res.status(300).json({message: "book not found"});
  });
+
+
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  
+  axios.get(`https://maddalas-5000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/isbn/${isbn}`)
+    .then((response) => {
+      const bookDetails = response.data.bookDetails;
+      if (bookDetails) {
+        return res.status(200).json({ message: "Book found", bookDetails });
+      } else {
+        return res.status(404).json({ message: "Book not found" });
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({ message: "Failed to fetch book details", error: error.message });
+    });
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -73,6 +104,24 @@ public_users.get('/author/:author',function (req, res) {
   
 });
 
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+  
+    // Make an HTTP GET request to fetch book details by author
+    axios.get(`https://maddalas-5000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/author/${author}`)
+      .then((response) => {
+        const books = response.data.books;
+        if (books.length > 0) {
+          return res.status(200).json({ message: "Author found, these are the list of book details", books });
+        } else {
+          return res.status(404).json({ message: "Author not found" });
+        }
+      })
+      .catch((error) => {
+        return res.status(500).json({ message: "Failed to fetch book details by author", error: error.message });
+      });
+  });
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
@@ -94,6 +143,25 @@ public_users.get('/title/:title',function (req, res) {
     
   return res.status(300).json({message: "title not found"});
 });
+
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+  
+    // Make an HTTP GET request to fetch book details by title
+    axios.get(`https://maddalas-5000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai//title/${title}`)
+      .then((response) => {
+        const books = response.data.books;
+        if (books.length > 0) {
+          return res.status(200).json({ message: "Title found, these are the list of book details", books });
+        } else {
+          return res.status(404).json({ message: "Title not found" });
+        }
+      })
+      .catch((error) => {
+        return res.status(500).json({ message: "Failed to fetch book details by title", error: error.message });
+      });
+});
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
